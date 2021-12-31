904343cbb6a0b902bc94ba4d3e2fcef8a7c8dda8
jotabass <jotabelk@gmail.com>
2021-12-31 08:22:06 -0300

    primeira alteração na tabuada
    agora posso escolher a função antes do calculo

--- src/tabuada.kt
+++ src/tabuada.kt
@@ -1,11 +1,30 @@
+
+
 fun main() {
     /**
      * tabuada de multiplicação simples.
+     * foi acrescentado calculo com os operadores simples
+     * dando a opção de escolha para somar ou dividir etc...
      */
-    println("Digite um numero ")
+
+    println("digite o operador desejado para tabuada sendo: soma,divisão,multiplicação e subtração ")
+    val operador  = readLine()!!.toString()
+    println("O operador escolhido foi ${operador}!")
+
+    println("Digite um valor para calculo ")
     val num  = readLine()!!.toInt()
-    println("Multiplicando o número: ${num}")
+    //println("Multiplicando o número: ${num}")
+
+
     for (num2 in 0..10){
-        println("${num} x ${num2} = ${num * num2}")
+        //if (operador == "multiplicacao")
+        //    println("${num} x ${num2} = ${num * num2}")
+       // if (operador == "soma")
+       //     println("${num} + ${num2} = ${num + num2}")
+        //if (operador == "subitracao")
+        //    println("${num} - ${num2} = ${num - num2}")
+        if (operador == "divisao")
+            println("${num} / ${num2} = ${num / num2}")
+
     }
 }
