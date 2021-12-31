0a2ed11dfd3d30af6ed6ee304a75410352c2fd6a
jotabass <jotabelk@gmail.com>
2021-12-31 14:46:43 -0300

    segunda alteração na tabuada
    foi removido a divisão pois não tem como dividir por 0

--- src/tabuada.kt
+++ src/tabuada.kt
@@ -1,30 +1,25 @@
-
-
+/**
+ * tabuada de multiplicação simples.
+ * foi acrescentado calculo com os operadores simples
+ * dando a opção de escolha para somar ou multiplicar e etc...
+ */
 fun main() {
-    /**
-     * tabuada de multiplicação simples.
-     * foi acrescentado calculo com os operadores simples
-     * dando a opção de escolha para somar ou dividir etc...
-     */
 
-    println("digite o operador desejado para tabuada sendo: soma,divisão,multiplicação e subtração ")
+    println("digite o operador desejado para tabuada sendo: soma, multiplicação e subtração ")
     val operador  = readLine()!!.toString()
     println("O operador escolhido foi ${operador}!")
 
     println("Digite um valor para calculo ")
     val num  = readLine()!!.toInt()
-    //println("Multiplicando o número: ${num}")
-
 
     for (num2 in 0..10){
-        //if (operador == "multiplicacao")
-        //    println("${num} x ${num2} = ${num * num2}")
-       // if (operador == "soma")
-       //     println("${num} + ${num2} = ${num + num2}")
-        //if (operador == "subitracao")
-        //    println("${num} - ${num2} = ${num - num2}")
-        if (operador == "divisao")
-            println("${num} / ${num2} = ${num / num2}")
+        if (operador == "multiplicacao")
+            println("${num} x ${num2} = ${num * num2}")
+        if (operador == "soma")
+            println("${num} + ${num2} = ${num + num2}")
+        if (operador == "subtracao")
+           println("${num} - ${num2} = ${num - num2}")
+
 
     }
 }
